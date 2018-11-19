769874f716a852cb50371f493c80f8a56b2b2f8c
psubbo <psubbo@yandex.ru>
2018-11-19 23:01:11 +0300

    Oauth & some refactoring done

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
-module.exports({
-    "googleClientID": "key"
-
-
-});
+module.exports = {
+    googleClientID: '901908692689-3954ddpn74fulqog8nliko4oqd574a6s.apps.googleusercontent.com',
+    googleClientSecret: 'jVpQ1-8j8ZaAmBPP7C4ESxzK'
+}
--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-/* passport.use(new GoogleStrategy({
-        clientID: GOOGLE_CLIENT_ID,
-        clientSecret: GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://www.example.com/auth/google/callback"
-    },
-    function(accessToken, refreshToken, profile, cb) {
-        User.findOrCreate({ googleId: profile.id }, function(err, user) {
-            return cb(err, user);
-        });
-    }
-)); */
-
+require('./services/passport')
 const app = express();
+require('./routes/authRoutes')(app);
+
+
+
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res) => {
-    res.send({ "hi": "there" });
-})
-
-
-app.listen(PORT, () => { console.log('App launched on port: ' + PORT) })
+app.listen(PORT, () => { console.log('App working on port: ' + PORT) })
