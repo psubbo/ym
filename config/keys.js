ac37827c9936ec4cdbb510d87a52b1ed084ef580
psubbo <psubbo@yandex.ru>
2018-11-22 23:23:49 +0300

    Proxy adjustment in google Oauth policy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-    googleClientID: '901908692689-3954ddpn74fulqog8nliko4oqd574a6s.apps.googleusercontent.com',
-    googleClientSecret: 'jVpQ1-8j8ZaAmBPP7C4ESxzK'
-}
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else {
+  module.exports = require('./dev');
+};
