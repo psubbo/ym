6f1a5efcac45af8cb1649bd9d9e5d826d183ca9d
psubbo <psubbo@yandex.ru>
2018-11-29 12:51:50 +0300

    minDate bug fix

--- client/src/components/Custom.js
+++ client/src/components/Custom.js
@@ -38,6 +38,7 @@ class Custom extends React.Component {
             selected={this.state.startDate}
             dateFormat="dd/MM/yyyy"
             className="dateInput"
+            maxDate={this.state.endDate}
             startDate={this.state.startDate}
             endDate={this.state.endDate}
             onChange={this.handleChangeStart}
@@ -56,6 +57,29 @@ class Custom extends React.Component {
             endDate={this.state.endDate}
             onChange={this.handleChangeEnd}
           />
+        </div>
+
+        <h3>Общая информация:</h3>
+        <div className="averageYtd">
+          <p>Средний балл (YTD):</p> <span />
+        </div>
+        <div className="averageMonth">
+          <p>Средний балл (последние 30 дней):</p> <span />
+        </div>
+        <div className="negativeToAnswerYtd">
+          <p>Негативных отзывов без ответа (YTD):</p> <span />
+        </div>
+        <div className="willRepurchaseYtd">
+          <p>Купили бы снова (YTD):</p> <span />
+        </div>
+        <div className="willRepurchaseMonth">
+          <p>Купили бы снова (последние 30 дней):</p> <span />
+        </div>
+        <div className="answerTimeYtd">
+          <p>Срок ответа на негатив (YTD):</p> <span />
+        </div>
+        <div className="answerTimeMonth">
+          <p>Срок ответа на негатив (предыдущие 30 дней):</p> <span />
         </div>
       </div>
     );
